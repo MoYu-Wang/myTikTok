139499f21367c7a55d917e6a011bc7bbef3354f7
Haosikai <1009979270@qq.com>
2024-04-14 17:52:00 +0800

    更改了message的颜色

--- web前端/public/comment.js
+++ web前端/public/comment.js
@@ -1,7 +1,7 @@
 //点击发布评论
 document.getElementById("publishComment").addEventListener("click",function(){
     if(!UserIsLogin()){
-        alert("用户未登录\n或登录信息已过期")
+        showMessage("用户未登录\n或登录信息已过期")
         return
     }
     //获取登录用户信息
@@ -14,11 +14,11 @@ document.getElementById("publishComment").addEventListener("click",function(){
     POST_Req("/video/comment",CommentVideoParam(userData.token,videoInfo.videoID,textareaContent))
     .then(data => {
         if (data.status_code != 0) {
-            alert(data.status_msg);
+            showMessage(data.status_msg);
             return;
         }
         UpdateComment()
-        alert("评论成功")
+        showMessage("评论成功")
         document.getElementById('commentText').value = '';
     })
     .catch(error => {
